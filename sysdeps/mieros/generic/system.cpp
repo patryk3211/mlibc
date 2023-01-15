d00f051f28b304b89d656430ebb739ab54f75d6b
patryk3211 <patrykmierzy@gmail.com>
2023-01-15 11:10:45 +0100

    Added fork and execve Fixed and change some other calls

--- sysdeps/mieros/generic/system.cpp
+++ sysdeps/mieros/generic/system.cpp
@@ -5,21 +5,43 @@
 
 namespace mlibc {
     void sys_exit(int code) {
-        SYSCALL(SYSCALL_EXIT, code);
+        syscall(SYSCALL_EXIT, code);
         while(true);
     }
 
     void sys_libc_log(char const* msg) {
-        SYSCALL(SYSCALL_WRITE, 2, msg, strlen(msg));
+        syscall(SYSCALL_WRITE, 2, msg, strlen(msg));
     }
 
     void sys_libc_panic() {
-        SYSCALL(SYSCALL_EXIT, -1);
+        syscall(SYSCALL_EXIT, -1);
         while(true);
     }
 
     int sys_clock_get(int clock, time_t *secs, long *nanos) {
         /// UNIMPLEMENTED
         return -1;
+    }
+
+    int sys_futex_wait(int *pointer, int expected, const struct timespec *time) {
+        /// UNIMPLEMENTED
+        return -1;
+    }
+
+    int sys_futex_wake(int *pointer) {
+        /// UNIMPLEMENTED
+        return -1;
+    }
+
+    int sys_fork(pid_t *child) {
+        int status = syscall(SYSCALL_FORK);
+        if(status >= 0) {
+            *child = status;
+            return 0;
+        } else return -status;
+    }
+
+    int sys_execve(const char *path, char *const argv[], char *const envp[]) {
+        return -syscall(SYSCALL_EXECVE, path, argv, envp);
     }
 }
--- sysdeps/mieros/generic/thread.cpp
+++ sysdeps/mieros/generic/thread.cpp
@@ -3,18 +3,7 @@
 #include <mieros/syscall.h>
 
 namespace mlibc {
-    int sys_futex_wait(int *pointer, int expected, const struct timespec *time) {
-        /// UNIMPLEMENTED
-        return -1;
-    }
-
-    int sys_futex_wake(int *pointer) {
-        /// UNIMPLEMENTED
-        return -1;
-    }
-
     int sys_tcb_set(void* ptr) {
-        /// UNIMPLEMENTED
-        return SYSCALL(SYSCALL_ARCH_PRCTL, ARCH_SET_TCB, (uintptr_t*)&ptr);
+        return -syscall(SYSCALL_ARCH_PRCTL, ARCH_SET_TCB, (uintptr_t*)&ptr);
     }
 }
